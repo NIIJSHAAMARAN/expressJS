d3eb4646cc821a51215e517b023ac2869c9905fd
NIJISHA <nijishaamaran@gmail.com>
2026-01-13 20:59:45 +0530

    display the first 5  products in asc order based on price

---
+++ product_list/routes/index.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+const product = require('../models/userModel');
+const { validationResult, check } = require('express-validator');
+
+/* GET user form page */
+router.get('/', function (req, res) {
+  res.render('product-form', { errors: [] });
+});
+
+/* POST create user with validation */
+router.post(
+  '/addProduct',
+  [
+    check('productname')
+      .notEmpty()
+      .withMessage('please enter product name'),
+
+    check('quantity')
+      .notEmpty().isNumeric()
+      .withMessage('please enter a valid quantity'),
+
+    check('price')
+      .notEmpty().isNumeric()
+      .withMessage('please enter a valid price'),
+  ],
+  function (req, res) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // Validation failed → re-render form with errors
+      return res.render('product-form', {
+        errors: errors.array(),
+        productname: req.body.productname,
+        quantity: req.body.quantity,
+        price:req.body.price,
+      });
+    }
+
+    // Validation success → save to DB
+    const {productname,quantity,price } = req.body;
+
+    const newProduct = new product({
+      productname,
+      quantity,
+      price,
+    });
+
+    newProduct
+      .save()
+      .then(() => {
+        res.render('form-data', {
+          message: 'Data Saved to product DB',
+          allData: req.body,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Database error');
+      });
+  }
+);
+// for getting data from db
+router.get('/getProduct', function (req,res) {
+    product.find({},'productname price').sort({price:'asc'}).limit(5).then(data => {
+      res.render('index', {data:data})
+
+//       Shows only the product name and price
+
+// Displays only the first 5 products
+
+    }).catch(error => {
+      console.error(error);
+      
+    });
+  });
+
+module.exports = router;
